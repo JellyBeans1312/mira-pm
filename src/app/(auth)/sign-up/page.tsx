7a511363017b0d42ac8f4dac25b0d1ed1d496b66
Aidan McKay <aidanmckay2000@gmail.com>
2024-10-31 14:27:23 -0500

    Add routes, page layouts,api configurtaions

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,18 @@
+import { redirect } from "next/navigation";
+
+import { SignUpCard } from "@/features/auth/components/sign-up-card";
+import { getCurrent } from "@/features/auth/actions";
+
+const SignUpPage = async () => {
+    const user = await getCurrent();
+
+    if(user) redirect('/');
+    
+    return (
+        <div>
+            <SignUpCard />
+        </div>
+    )
+};
+
+export default SignUpPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Inter } from 'next/font/google';
+
+import { cn } from "@/lib/utils";
+import { QueryProvider } from "@/components/query-provider";
+import { Toaster } from "@/components/ui/sonner";
+
 import "./globals.css";
+const inter = Inter({ subsets: ['latin'] });
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +33,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(inter.className, 'antialiased min-h-screen')}
       >
-        {children}
+        <QueryProvider>
+          <Toaster />
+          {children}
+        </QueryProvider>
       </body>
     </html>
   );
