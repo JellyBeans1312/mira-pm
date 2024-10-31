a6a401261966684b4fc79054e6d16b7638a4892b
Aidan McKay <aidanmckay2000@gmail.com>
2024-10-31 14:26:35 -0500

    Changes to UI/UX

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+import Link from "next/link"
+import { DottedSeparator } from "./dotted-separator"
+import { Navigation } from "./navigation"
+import { WorkspaceSwitcher } from "./workspace-switcher"
+export const Sidebar = () => {
+    return (
+        <aside className="h-full bg-neutral-100 p-4 w-full">
+            <Link href={'/'}>
+                <Image src={'logo.svg'} alt="logo" width={56} height={56}/>
+            </Link>
+            <DottedSeparator  className="my-4"/>
+            <WorkspaceSwitcher />
+            <DottedSeparator  className="my-4"/>
+            <Navigation/>
+        </aside>
+    )    
+}
