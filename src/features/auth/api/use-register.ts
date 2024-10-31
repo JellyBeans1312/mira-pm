ad3914e051434a8a4c7e491f0127348f52a826d9
Aidan McKay <aidanmckay2000@gmail.com>
2024-10-31 14:28:23 -0500

    Handle main app UI/UX logic and specific feature components

---
+++ src/features/auth/api/use-register.ts
@@ -0,0 +1,34 @@
+import { useMutation } from '@tanstack/react-query';
+import { InferRequestType, InferResponseType } from 'hono';
+import { useRouter } from 'next/navigation';
+import { useQueryClient } from '@tanstack/react-query';
+import { client } from '@/lib/rpc';
+
+type ResponseType = InferResponseType<typeof client.api.auth.register["$post"]>;
+type RequestType = InferRequestType<typeof client.api.auth.register["$post"]>;
+
+export const useRegister = () => {
+    const router = useRouter();
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation<
+        ResponseType,
+        Error,
+        RequestType
+    >({
+        mutationFn: async ({ json }) => {
+            const response = await client.api.auth.register.$post({ json });
+
+            if(!response.ok) {
+                throw new Error('Failed to sign up')
+            }
+
+            return await response.json();
+        },
+        onSuccess: () => {
+            router.refresh();
+            queryClient.invalidateQueries({ queryKey: ['current'] })
+        }
+    });
+    return mutation;
+};
---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,137 @@
+"use client";
+import { DottedSeparator } from '@/components/dotted-separator';
+import { Button } from '@/components/ui/button';
+import  {
+    Card,
+    CardHeader,
+    CardContent,
+    CardTitle
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import {
+    Form,
+    FormField,
+    FormItem,
+    FormControl,
+    FormMessage,
+} from '@/components/ui/form';
+import { loginSchema } from '@/features/auth/schemas';
+import { useLogin } from '@/features/auth/api/use-login';
+
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Link from 'next/link';
+
+
+
+
+export const SignInCard = () => {
+    const { mutate, isPending } = useLogin();
+    
+    const form = useForm<z.infer<typeof loginSchema>>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        }
+    });
+
+    const onSubmit = (values: z.infer<typeof loginSchema>) => {
+        mutate({ json: values });
+    }
+    
+
+    return (
+        <Card className='w-full h-full md:w-[486px] border-none shadow-none'>
+            <CardHeader className='flex items-center justify-center text-center p-7'>
+                <CardTitle className='text-2xl'>
+                    Welcome Back!
+                </CardTitle>
+            </CardHeader>
+            <div className='px-7'>
+                <DottedSeparator />
+            </div>
+            <CardContent className='p-7'>
+                <Form {...form}>
+                    <form  onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
+                        <FormField
+                            name="email"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input 
+                                            type='email'
+                                            placeholder='Enter email address'
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            name="password"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input 
+                                            type='password'
+                                            placeholder='Enter password'
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        >
+                        </FormField>
+                        <Button 
+                            className='w-full'
+                            disabled={isPending}
+                            size={"lg"}
+                        >
+                            Login
+                        </Button>
+                    </form>
+                </Form>
+            </CardContent>
+            <div className='px-7'>
+                <DottedSeparator />
+            </div>
+            <CardContent className='p-7 flex flex-col gap-y-4'>
+                <Button
+                    disabled={isPending}
+                    variant={'secondary'}
+                    size={'lg'}
+                    className='w-full'
+                >
+                    <FcGoogle className='mr-2 size-5' />
+                    Login with Google
+                </Button>
+                <Button
+                    disabled={isPending}
+                    variant={'secondary'}
+                    size={'lg'}
+                    className='w-full'
+                >
+                    <FaGithub className='mr-2 size-5' />
+                    Login with GitHub
+                </Button>
+            </CardContent>
+            <div className='px-7'>
+                <DottedSeparator />
+            </div>
+            <CardContent className='p-7 flex items-center justify-center'>
+                Don't have an account yet?
+                <Link href={'/sign-up'}>
+                    <span className='text-blue-700'> &nbsp; Sign Up </span>
+                </Link>
+            </CardContent>
+        </Card>
+    );
+};
