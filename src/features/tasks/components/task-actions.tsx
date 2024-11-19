8a32670ff682ee49540053a423a3056998eeb974
Aidan McKay <aidanmckay2000@gmail.com>
2024-11-19 14:40:50 -0600

    Edit task form, wrapper, modal, and task action functionality

--- src/features/tasks/components/task-actions.tsx
+++ src/features/tasks/components/task-actions.tsx
@@ -7,6 +7,13 @@ import {
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 
+import { useDeleteTask } from '@/features/tasks/api/use-delete-task';
+
+import { useConfirm } from '@/hooks/use-confirm';
+import { useRouter } from 'next/navigation';
+import { useWorkspaceId } from '@/features/workspaces/hooks/use-workspace-id';
+import { useEditTaskModal } from '../hooks/use-edit-task-modal';
+
 interface TaskActionProps {
     id: string;
     projectId: string;
@@ -18,40 +25,68 @@ export const TaskActions = ({
     projectId,
     children
 }: TaskActionProps) => {
+    const router = useRouter();
+    const workspaceId = useWorkspaceId();
+
+    const { open } = useEditTaskModal();
+
+    const [ConfirmationDialog, confirm] = useConfirm(
+        'Delete Task',
+        'Are you sure? This action cannot be undone.',
+        'destructive'
+    );
+    
+    const { mutate: deleteTask, isPending } = useDeleteTask();
+
+    const handleDelete = async () => {
+        const ok = await confirm()
+        if(!ok) return;
+
+        deleteTask({ param: { taskId: id }})
+    };
+
+    const onOpenTask = () => {
+        router.push(`/workspaces/${workspaceId}/tasks/${id}`)
+    }
+    const onOpenProject = () => {
+        router.push(`/workspaces/${workspaceId}/projects/${projectId}`)
+    }
+
     return (
         <div className='flex justify-end'>
+            <ConfirmationDialog />
             <DropdownMenu modal={false}>
                 <DropdownMenuTrigger asChild>
                     {children}
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align='end' className='w-48'>
                     <DropdownMenuItem 
-                        onClick={() => {}} 
-                        disabled={false} 
+                        onClick={onOpenTask} 
+
                         className='font-medium p-[10px]'
                     >
                         <ExternalLinkIcon  className='size-4 mr-2 stroke-2'/>
                         Task Details
                     </DropdownMenuItem>
                     <DropdownMenuItem 
-                        onClick={() => {}} 
-                        disabled={false} 
+                        onClick={onOpenProject} 
+
                         className='font-medium p-[10px]'
                     >
                         <ExternalLinkIcon  className='size-4 mr-2 stroke-2'/>
                         Open Project
                     </DropdownMenuItem>
                     <DropdownMenuItem 
-                        onClick={() => {}} 
-                        disabled={false} 
+                        onClick={() => open(id)} 
+
                         className='font-medium p-[10px]'
                     >
                         <PencilIcon  className='size-4 mr-2 stroke-2'/>
                         Edit Task
                     </DropdownMenuItem>
                     <DropdownMenuItem 
-                        onClick={() => {}} 
-                        disabled={false} 
+                        onClick={handleDelete} 
+                        disabled={isPending} 
                         className='text-amber-700 focus:text-amber-700 font-medium p-[10px]'
                     >
                         <TrashIcon  className='size-4 mr-2 stroke-2'/>
